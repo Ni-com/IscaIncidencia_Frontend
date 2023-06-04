621e2bbf742e03b31204ee79049301974342f1ad
Ni-com <116836889+Ni-com@users.noreply.github.com>
2023-06-04 10:36:53 +0200

    Add files via upload

---
+++ IscaIncidencia.Android/Helper/FileSystemServiceAndroid.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using IscaIncidencia.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IscaIncidencia.Droid.Helper
+{
+    public class FileSystemServiceAndroid : IFileSystemService
+    {
+        public string GetPublicExternalStoragePath()
+        {
+            return Android.OS.Environment.ExternalStorageDirectory.AbsolutePath;
+        }
+    }
+}
